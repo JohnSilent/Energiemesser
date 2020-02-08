20c8cc39672c4f86c60858794f1e0ba095feedd2
Brian Hansen <brian.hansen@gmx.de>
2020-02-08 01:57:17 +0100

    1) Funktion get_input_keys durch .keys() ersetzt. 2) read_input_values: - while loop durch for-loop ersetzt - isinstance für typprüfungen eingesetzt

--- Strommesser.py
+++ Strommesser.py
@@ -3,14 +3,16 @@ import minimalmodbus
 
 
 class Resource_Monitor:
+
     def __init__(self):
         self.instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
         self.instrument.serial.baudrate = 9600
         self.instrument.serial.timeout = 0.2
         self.instrument.mode = minimalmodbus.MODE_RTU
         self.data = {}
-        self.input_register = {"Voltage": {
-            "port": 0, "digits": 2, "Unit": "V", "use": True},
+        self.input_register = {
+            "Voltage":
+                {"port": 0, "digits": 2, "Unit": "V", "use": True},
             "Ampere":
                 {"port": 6, "digits": 2, "Unit": "A", "use": True},
             "Wirkleistung":
@@ -56,44 +58,39 @@ class Resource_Monitor:
             "CurrentResettableTotalActiveEnergy":
                 {"port": 384, "digits": 2, "Unit": "kWh", "use": False},
             "CurrentResettableTotalReactiveEnergy":
-                {"port": 386, "digits": 2, "Unit": "kVArh", "use": False}
-        }
+                {"port": 386, "digits": 2, "Unit": "kVArh", "use": False}}
 
-    def read_input_values(self, input_register_keys=None):
+
+    def read_input_values(self):
         """
         Read all in self.input_register defined data points and stored the result as float value
         into self.data dictionary
         :return: self.data dictionary
         """
 
-        if input_register_keys is None:
-            input_register_keys = self.get_input_keys()
-        if self.instrument is not None:
-            for key in input_register_keys:
-                
+        if isinstance(self.instrument, minimalmodbus.Instrument):
+            for key in self.input_register.keys():
                 if self.input_register[key]["use"] is True:
-                    fehler = 0
-                    while True:  # Anzahl der Versuche
+                    for fehler in range(1, 6):
                         try:
                             messwert = self.instrument.read_float(functioncode=4,
                                                                   registeraddress=self.input_register[key]["port"],
                                                                   number_of_registers=self.input_register[key]["digits"])
                         except OSError:
-                            fehler += 1
                             print("Kommunikationserror Nr. {}".format(fehler))
-                            if fehler > 5:  # Anzahl der Versuche
+                            if fehler == 5:
                                 raise OSError
                         else:
                             break
 
-                    if messwert is None:
+                    if not isinstance(messwert, float):
                         print("Value '{}' not available".format(key))
                     else:
                         # TODO: messwertrunden abhängig des Registers machen
                         self.data[key] = round(messwert, 4)
-                    print("Value '{}' = '{}' '{}'".format(key, self.data[key],self.input_register[key]["Unit"]))
+                    print("Value '{}' = '{}' '{}'".format(key, self.data[key], self.input_register[key]["Unit"]))
                 else:
-                    #print("Value '{}' not used!".format(key))
+                    # Register nicht in self.input_register
                     pass
 
         else:
@@ -103,14 +100,6 @@ class Resource_Monitor:
 
         return self.data
 
-
-    def get_input_keys(self):
-        """
-        Hilfsmethode zur Erstellung der Intervallklassen
-        :return:
-        """
-        input_register_keys = [key for key in self.input_register]
-        return input_register_keys
 
 
 if __name__ == '__main__':
